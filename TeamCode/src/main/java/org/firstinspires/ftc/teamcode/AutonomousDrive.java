4fc5364cc7cd7a5bdbf55aa6597c8fc92d36d1a3
AlexanderSwann <32579236+AlexanderSwann@users.noreply.github.com>
2018-10-24 09:35:12 -0400

    Update TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDrive.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDrive.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@Autonomous
+
+public class AutonomousDrive extends LinearOpMode {
+    public DcMotor leftMotor = null;
+    public DcMotor rightMotor = null;
+
+    @Override
+    public void runOpMode() {
+        leftMotor = hardwareMap.dcMotor.get("ld");
+        rightMotor = hardwareMap.dcMotor.get("rd");
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+            rightMotor.setPower(100);
+            leftMotor.setPower(100);
+            telemetry.addData("Status", "Forward");
+            telemetry.update();
+            sleep(2500);
+            rightMotor.setPower(0);
+            leftMotor.setPower(0);
+            telemetry.addData("Status", "Stop");
+            sleep(100);
+            rightMotor.setPower(-100);
+            leftMotor.setPower(-100);
+            telemetry.addData("Status", "back");
+            telemetry.update();
+            sleep(200);
+            rightMotor.setPower(-100);
+            leftMotor.setPower(0);
+            telemetry.addData("Status", "right");
+            sleep(200);
+            rightMotor.setPower(100);
+            leftMotor.setPower(100);
+            telemetry.addData("Status", "Forward");
+            telemetry.update();
+            sleep(6400);
+            rightMotor.setPower(0);
+            leftMotor.setPower(0);
+            telemetry.addData("Status", "Stop");
+            break;
+
+        }
+    }
+}
