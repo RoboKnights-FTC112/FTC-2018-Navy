77b9a2d6ffe5cd49237afec4f11f4b81640311e5
AlexanderSwann <32579236+AlexanderSwann@users.noreply.github.com>
2018-11-08 20:00:07 -0500

    Update TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboKnightsNavy.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboKnightsNavy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboKnightsNavy.java
@@ -10,7 +10,7 @@ public class RoboKnightsNavy extends LinearOpMode {
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorVeiwId);
         params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         params.vuforiaLicenseKey= "AfNu6or/////AAABmbz3RBzAq0E9lLg95aebMBM2Er4MR6IvcC26uRVkA8M7SqU+48qaf2KciR2LRYOkQv1L1KWo3EWY+ALYOBdHyP5oro61RsCW24FOXYhooU2QdYVk28MQfU8dUvG1NDMJ8zN/5RnM3fMPfMqEtZ+4hluqGNCjTdwzrecN9ZpAtDpTpIQ4V5eN9M1YYyNfT9QJRswL7PdyZ4jDC70w+KNtx5U6iRJNKLira1dzBvoGZjGDBRoU9w+m1RVFzgUHVBDGQ85BQKlXNUDUcJKW1e9/2B6YkAYr38kSTQw1FK3ywpJ9FwtKOUw8ByBf96n7ScWnC72l2keQQ6tsh6xsZioxaXO8WOBO4ukoFWuevJOzFLn+";
-        params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.cameraMonitorFeedback.AXES;
+        params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
 
         VuforiaLocalizer vuforia = ClassFactory.createVuforiaLocalizer(params);
         Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
@@ -29,7 +29,7 @@ public class RoboKnightsNavy extends LinearOpMode {
                 VectorF translation pose.getTranslation();
 
                 telemetry.addData(beac.getName() + "-Translate", translation );
- 
+
                 double degreesToTurn = Math.toDegrees(Math.atan2(translation.get(1), translation.get(2)));
 
                 telemetry.addData(beac.getName() + "-Degrees", degreesToTurn );
